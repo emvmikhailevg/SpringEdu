90e0c2012e68da025866eb03afe585a1372b509c
mikhailemv <cheburusshka0303@gmail.com>
2023-09-12 15:37:05 +0500

    Spring Framework. Урок 19: Параметры GET запроса. Аннотация @RequestParam

--- src/main/java/ru/emelianov/springwebapp/controllers/FirstController.java
+++ src/main/java/ru/emelianov/springwebapp/controllers/FirstController.java
@@ -3,13 +3,15 @@ package ru.emelianov.springwebapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/main")
-    public String mainPage() {
+    public String mainPage(@RequestParam(value = "name", required = false) String name,
+                           @RequestParam(value = "surname", required = false) String surname) {
         return "first/main_page";
     }
 
