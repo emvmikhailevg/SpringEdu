1d3a7b945d717139ed7027c550a1db9bf21257d0
mikhailemv <cheburusshka0303@gmail.com>
2023-09-26 12:23:07 +0500

    Spring Framework. Урок 23: CRUD приложение. PATCH, DELETE запросы

--- src/main/java/ru/emelianov/springwebapp/dao/PersonDAO.java
+++ src/main/java/ru/emelianov/springwebapp/dao/PersonDAO.java
@@ -36,4 +36,13 @@ public class PersonDAO {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person updatedPerson) {
+        var personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
 }
